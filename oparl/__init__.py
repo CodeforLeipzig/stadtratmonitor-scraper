e2f2023e9f0f462e94072e6ffd947f7912cc2674
EO <saxonaut@gmx.de>
2023-09-02 17:52:06 +0200

    buffered oparl class

--- oparl/__init__.py
+++ oparl/__init__.py
@@ -1,38 +1,63 @@
+from collections import deque
 import oparl.fakerequest as request
 from oparl.oparl_factory import Factory
-from oparl.oparl_objects import Basic, Paper, Person, Organization, Location, Membership
+from oparl.oparl_objects import \
+    Basic, Paper, Person, Organization, Location, Membership, Consultation
 
 
-START_URL = request.START_URL
+class Oparl:
+    START_URL = request.START_URL
+    MAX_PAGES = 10
+    BUFFER_SIZE = 1000
 
-Factory.request = request
-Factory.mapping = {None: Basic,
-                   "https://schema.oparl.org/1.1/Paper": Paper,
-                   "https://schema.oparl.org/1.1/Person": Person,
-                   "https://schema.oparl.org/1.1/Organization": Organization,
-                   "https://schema.oparl.org/1.1/Location": Location,
-                   "https://schema.oparl.org/1.1/Membership": Membership}
+    buffer = deque([], BUFFER_SIZE)
+    factory = Factory
+    factory_mapping = {None: Basic,
+                       "https://schema.oparl.org/1.1/Paper": Paper,
+                       "https://schema.oparl.org/1.1/Person": Person,
+                       "https://schema.oparl.org/1.1/Organization": Organization,
+                       "https://schema.oparl.org/1.1/Location": Location,
+                       "https://schema.oparl.org/1.1/Membership": Membership,
+                       "https://schema.oparl.org/1.1/Consultation": Consultation}
 
-
-class Pagination:
-    def __init__(self, start_url=START_URL, max_pages=5):
-        self.start_url = start_url
-        self.max_pages = max_pages
-
-    def __iter__(self):
-        max_pages = self.max_pages
-        url = self.start_url
+    @classmethod
+    def pagination(cls):
+        max_pages = cls.MAX_PAGES
+        url = cls.START_URL
+        factory = cls.factory
         page_count = 0
+
         while url and page_count < max_pages:
             page = request.get(url)
             page_count += 1
             for item in page.get('data'):
-                yield Factory.fabricate(item)
+                yield factory.fabricate(item)
                 url = page.get('links')
                 if url:
                     url = url.get('next')
 
+    @classmethod
+    def _wrap_oparl(cls, _: Factory, item):
+        if isinstance(item, dict):
+            o_cls = cls.factory_mapping.get(item.get('type'))
+            o_obj = o_cls(item)
+            cls.buffer.append(o_obj)
+            return o_obj
 
-if __name__ == '__main__':
-    for item in Pagination():
-        print(item)
+    @classmethod
+    def _ask_buffer(cls, _: Factory, item):
+        if isinstance(item, str) and item.startswith('http'):
+            for buffered_item in cls.buffer:
+                if item == buffered_item.oparl_id:
+                    return buffered_item
+
+    @classmethod
+    def _retrieve_from_api(cls, factory: Factory, item):
+        if isinstance(item, str) and item.startswith('http'):
+            result = request.get(item)
+            result = factory.fabricate(result)
+            cls.buffer.append(result)
+            return result
+
+
+Oparl.factory.add_function(Oparl._wrap_oparl, Oparl._ask_buffer, Oparl._retrieve_from_api)
