419c65e2020f61e3918230c98761552c6fe094bf
EO <saxonaut@gmx.de>
2023-09-03 04:11:07 +0200

    relationships complete update

--- scraping.py
+++ scraping.py
@@ -1,27 +1,45 @@
 from neo_connector import database_connection, Session
+from nodes_scheme import BasicNodeInterface, DbRelation
 from nodes_from_oparl import node_factory as oparl_node_factory
 from statements import create_relation
-from nodes_from_oparl import UnknownOparlNode
 from oparl import Oparl
+from collections import deque
+
+
+def update_nodes_and_relations(tx, node: BasicNodeInterface, buffer=None, /):
+    _buffer = buffer if buffer else deque()
+    for relation in node.relations():
+        relation: DbRelation
+        if relation in _buffer:
+            continue
+        else:
+            result = create_relation(tx, relation)
+            print(result)
+            _buffer.appendleft(relation)
+            for item in (relation.source, relation.target):
+                item: BasicNodeInterface
+                update_nodes_and_relations(tx, item, _buffer)
 
 
 def scrapping(db_con, oparl_):
     for item in oparl_.pagination():
         paper_node = oparl_node_factory(item)
+
         with db_con.session() as session:
             session: Session
-            #db_node: AbcOparlPaperInterface = session.execute_write(retrieve_single, node_paper)
-            #if db_node.modified == node_paper.modified: continue
+            # db_node: AbcOparlPaperInterface = session.execute_write(retrieve_single, node_paper)
+            # if db_node.modified == node_paper.modified: continue
+
+            session.execute_write(update_nodes_and_relations, paper_node, None)
+
+            '''
             for director in paper_node.directors():
-                if isinstance(director.source, UnknownOparlNode) or isinstance(director.target, UnknownOparlNode):
-                    continue
                 r = session.execute_write(create_relation, director)
                 pass
             for originator in paper_node.originators():
-                if isinstance(originator.source, UnknownOparlNode) or isinstance(originator.target, UnknownOparlNode):
-                    continue
                 r = session.execute_write(create_relation, originator)
                 pass
+            '''
 
 
 if __name__ == '__main__':
@@ -29,6 +47,5 @@ if __name__ == '__main__':
         oparl = Oparl()
         scrapping(dbc, oparl)
 
-
-#print(connection.driver.execute_query('match (n) return count(n)'))
-#connection.driver.close()
+# print(connection.driver.execute_query('match (n) return count(n)'))
+# connection.driver.close()
