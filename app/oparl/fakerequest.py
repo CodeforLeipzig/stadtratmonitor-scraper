a7ff7086884916a522d1eec98d118121424a9b7d
EO <saxonaut@gmx.de>
2023-10-22 22:37:45 +0200

    implement fake request for html

--- app/oparl/fakerequest.py
+++ app/oparl/fakerequest.py
@@ -13,3 +13,12 @@ def get(url):
             return json.loads(data[0])
         else:
             return {'id': url, 'deleted': True}
+
+
+class FakeHtmlRequest:
+    @classmethod
+    def get(cls, url) -> bytes:
+        with connect(DATABASE_NAME) as con:
+            res = con.execute('SELECT data FROM mirror WHERE id=?', (url,))
+            data = res.fetchone()
+            return data[0] if data else b''
