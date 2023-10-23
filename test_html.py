72baa908c5de58a1ce8d128fea7900514688fe08
EO <saxonaut@gmx.de>
2023-10-23 22:52:52 +0200

    test

--- test_html.py
+++ test_html.py
@@ -1,6 +1,7 @@
 from app.oparl.fakerequest import FakeHtmlRequest as request
-from app.html_scraper._sectionalize_html import process_html_page
-from app.html_scraper._header_processes import processes
+from app.html_scraper._sectionalize_html import HtmlSections
+
+# from app.html_scraper._header_processes import processes
 
 if __name__ == '__main__':
     examples = ['https://ratsinformation.leipzig.de/allris_leipzig_public/vo020?VOLFDNR=2012504',
@@ -12,8 +13,6 @@ if __name__ == '__main__':
 
     for e in examples:
         page = request.get(e)
-        for section in process_html_page(page):
-            process = processes.get(section.title)
-            if process:
-                [print(chunk) for chunk in process(section.content)]
-            print(section)
+        html_sections = HtmlSections(page)
+        [print(head) for head in html_sections.headers()]
+        [print(docpart) for docpart in html_sections.docparts()]
