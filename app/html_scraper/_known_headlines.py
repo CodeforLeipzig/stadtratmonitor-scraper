1b922268d4f4c1fbf261e2aa551b16943dc70151
EO <saxonaut@gmx.de>
2023-10-23 22:51:45 +0200

    add known vocab

---
+++ app/html_scraper/_known_headlines.py
@@ -0,0 +1,50 @@
+HEADERS = ('Betreff',
+           'Status',
+           'Vorlageart',
+           'Federführend',
+           'Einreicher',
+           'Vorlageanlass',
+           'Beteiligt')
+
+DEFAULT_HEADLINE = 'Sachverhalt'
+
+HEADLINES = (DEFAULT_HEADLINE,
+             'Sachverhalt:',
+             'Antwort',
+             'Antwort:',
+             'Beschluss',
+             'Beschlussvorschlag',  # css HeaderTitle
+             'Begründung des Antrags',
+             'Auswirkungen auf den Stellenplan',
+             'Finanzielle Auswirkungen',
+             'Im Haushalt wirksam',
+             'Folgekosten Einsparungen wirksam',
+             'Räumlicher Bezug'
+             'Zusammenfassung',
+             'Finanz. Auswirkung',  # css HeaderTitle
+             'Finanzielle Auswirkungen',  # checkboxes
+             'Im Haushalt wirksam',  # checkboxes
+             'Folgekosten Einsparungen wirksam',  # checkboxes
+             'Steuerrechtliche Prüfung',  # checkboxes
+             'Auswirkungen auf den Stellenplan',  # checkboxes
+             'Der gemäß Ursprungsantrag gefasste Beschluss wäre',
+             'Hintergrund zum Beschlussvorschlag:',
+             'Welche strategischen Ziele werden mit der Maßnahme unterstützt?',
+             '2030 – Leipzig wächst nachhaltig!',
+             'Ziele und Handlungsschwerpunkte',
+             'Klimawirkung durch den Beschluss der Vorlage',
+             'Stufe 1: Grobe Einordnung zur Klimawirkung (Klimaschutzes und zur –wandelanpassung),'
+             'Stufe 2: Die Vorlage berücksichtigt die zentralen energie- und klimapolitischen Beschlüsse',
+             'Stufe 3: Detaillierte Darstellung zur abschätzbaren Klimawirkung nur bei erheblicher Relevanz',
+             'Beschreibung des Abwägungsprozesses:',
+             'I. Eilbedürftigkeitsbegründung',
+             'II. Begründung Nichtöffentlichkeit',
+             'III.  Strategische Ziele',
+             'IV. Sachverhalt',
+             '1. Begründung Kreuz auf dem Deckblatt',
+             'Begründung',
+             'Begründung:'
+             '2. Realisierungs- / Zeithorizont (entfällt bei Ablehnung des Antrags)',
+             'Zusammenfassung',
+             'Rechtliche Konsequenzen'
+             )
